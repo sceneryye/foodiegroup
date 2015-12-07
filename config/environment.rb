2526d8600a2284b4344e6b340055c97d7c9690b2
存在的虚无 <killernova@126.com>
2015-12-07 12:48:09 +0800

    modify PIC_PATH

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 RailsOnForum::Application.initialize!
 
-PIC_PATH= "/root/code/foodie/public"
+PIC_PATH= "/root/code/foodiegroup/public"
