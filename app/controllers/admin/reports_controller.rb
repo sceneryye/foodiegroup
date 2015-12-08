c05e0e2958c6ba08c5e06767d79b8e6eb80b3d10
存在的虚无 <enternicky@126.com>
2015-12-08 13:39:16 +0800

    fix bug

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ReportsController < ApplicationController
   before_action :autheorize_admin!
   def index
-    @user = User.all.paginate(per_page: 20, page: params[:page]).order(id: :asc)
+    @user = User.paginate(per_page: 20, page: params[:page]).order(id: :asc)
   end
 end
