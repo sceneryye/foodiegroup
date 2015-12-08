bbc8950e1012f9b07911276cddafecc9bb170d4c
存在的虚无 <enternicky@126.com>
2015-12-08 15:08:36 +0800

    add css of admin/index

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -2,5 +2,6 @@ class Admin::ReportsController < ApplicationController
   before_action :autheorize_admin!
   def index
     @user = User.paginate(per_page: 20, page: params[:page]).order(id: :asc)
+    render layout: false
   end
 end
