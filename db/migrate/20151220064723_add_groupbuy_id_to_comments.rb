35f6962ad2069ee11a47738b49a9a2efbb83b905
存在的虚无 <enternicky@126.com>
2015-12-23 18:45:20 +0800

    bug

--- db/migrate/20151220064723_add_groupbuy_id_to_comments.rb
+++
@@ -1,7 +0,0 @@
-class AddGroupbuyIdToComments < ActiveRecord::Migration
-	def change
-	  	change_table(:comments) do |t|
-		  	t.references :groupbuy
-		end
-	end
-end
--- db/migrate/20151220064724_add_groupbuy_id_to_participants.rb
+++
@@ -1,7 +0,0 @@
-class AddGroupbuyIdToParticipants < ActiveRecord::Migration
-	def change
-	  	change_table(:participants) do |t|
-		  	t.references :groupbuy
-		end
-	end
-end
