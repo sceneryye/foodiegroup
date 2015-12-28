546aa28e93aa8ef3511337b98693aa04c3920301
叶思杰 <enternicky@126.com>
2015-12-28 19:52:24 +0800

    add timber

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,35 @@
+//= require foundation-datetimepicker
+
+//= require ckeditor/init
+
+//倒计时
+
+$('.groupbuy-list').each(function(){
+  var endTime = $(this).data('endtime');
+  var that = $(this);
+  window.setInterval(function(){
+    var EndTime= endTime;
+    var NowTime = new Date();
+    var t =EndTime * 1000 - NowTime.getTime();
+    if(t < 0) {
+      console.log(t);
+      var over = $('.events-index').data('over');
+      $(that).find('.countdown').text(over);
+      return;
+    }
+
+    var d=Math.floor(t/1000/60/60/24);
+    var h=Math.floor(t/1000/60/60%24);
+    var m=Math.floor(t/1000/60%60);
+    var s=Math.floor(t/1000%60);
+
+    that.find('.countdown-day').text(d);
+    that.find('.countdown-hour').text(h + ':');
+    that.find('.countdown-minute').text(m + ':');
+    that.find('.countdown-second').text(s);
+  }, 1000)
+
+  
+  
+  //setInterval(getRTime,1000);
+});
