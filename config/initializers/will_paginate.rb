381da3a88f21f7ca2e66a6565050f21a22e1a2d1
killernova <enternicky@126.com>
2016-01-04 12:57:33 +0800

    i18n

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,3 +1,3 @@
 #encoding:utf-8
-WillPaginate::ViewHelpers.pagination_options[:previous_label ] =  "前一页"
-WillPaginate::ViewHelpers.pagination_options[:next_label ] =  "后一页"
+WillPaginate::ViewHelpers.pagination_options[:previous_label ] =  t(:pre_page)
+WillPaginate::ViewHelpers.pagination_options[:next_label ] =  t(:next_page)
