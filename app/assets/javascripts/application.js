9dde6e3dc598cfd05f71b0de99bd77543d45be18
killernova <enternicky@126.com>
2016-01-07 21:49:52 +0800

    bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@ $(function(){ $(document).foundation(); });
 //   });
 
 
-$('.submit-buuton').on('click', function(){
+$('.submit-button').on('click', function(){
   $(this).parent().submit();
 })
