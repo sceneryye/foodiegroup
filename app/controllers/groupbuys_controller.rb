bc495005c1513854340d8aa78d7377544ce4f3c0
Scenery Ye <scenery.ye@gmail.com>
2016-01-08 08:50:51 +0800

    fileupload not finished

--- app/controllers/groupbuys_controller.rb
+++ app/controllers/groupbuys_controller.rb
@@ -29,43 +29,44 @@ before_action :validate_user!, only: [:new, :edit, :update, :create, :destroy]
 
    #@amount = Foodie::Participant.where
 
-   if signed_in? 
+    if signed_in? 
      @plus_menu = [{name: '<i class="fa  fa-comment"></i>'.html_safe+' '+t(:new_comment), path: new_groupbuy_comment_path(@groupbuy)},
       {name: '<i class="fa fa-user-plus"></i>'.html_safe+' '+t(:buy), path: new_groupbuy_participant_path(@groupbuy)}
-    ]
-    if @participants.where(:user_id => current_user.id).size>0
-      @again = '再次'     
-    else
-      @again = '立即'
+      ]
+      if @participants.where(:user_id => current_user.id).size>0
+        @again = '再次'     
+      else
+        @again = '立即'
+      end
     end
+
   end
 
-end
+  def new
+   @groupbuy = Groupbuy.new
+   session[:pic_file] = nil
+  end
 
-def new
- @groupbuy = Groupbuy.new
-end
+  def create
+    @groupbuy = Groupbuy.new(groupbuy_params)
+    @groupbuy.user = current_user
+    @groupbuy.locale = session[:locale]
 
-def create
-  @groupbuy = Groupbuy.new(groupbuy_params)
-  @groupbuy.user = current_user
-  @groupbuy.locale = session[:locale]
-
-  uploaded_io = params[:file]
-  if !uploaded_io.blank?
-    extension = uploaded_io.original_filename.split('.')
-    filename = "#{Time.now.strftime('%Y%m%d%H%M%S')}.#{extension[-1]}"
-    filepath = "#{PIC_PATH}/groupbuys/#{filename}"
-    File.open(filepath, 'wb') do |file|
-      file.write(uploaded_io.read)
-    end
-        # groupbuy_params.merge!(:pic_url=>"/groupbuys/#{filename}")
-        @groupbuy.pic_url = "/groupbuys/#{filename}"
-      end
-
-    #  return render :text=>  groupbuy_params
-
-
+    # uploaded_io = params[:file]
+    # if !uploaded_io.blank?
+    #   extension = uploaded_io.original_filename.split('.')
+    #   filename = "#{Time.now.strftime('%Y%m%d%H%M%S')}.#{extension[-1]}"
+    #   filepath = "#{PIC_PATH}/groupbuys/#{filename}"
+    #   File.open(filepath, 'wb') do |file|
+    #     file.write(uploaded_io.read)
+    #   end
+    #       # groupbuy_params.merge!(:pic_url=>"/groupbuys/#{filename}")
+    #       @groupbuy.pic_url = "/groupbuys/#{filename}"
+    # end
+     groupbuy_params.merge!(session[:pic_file])
+     session[:pic_file] = nil
+     
+     return render :text=>  groupbuy_params
 
     if @groupbuy.save
       post_url = "http://www.trade-v.com/send_group_message_api"
@@ -136,6 +137,17 @@ def create
   end
 
   def upload
+     uploaded_io = params[:file]
+    if !uploaded_io.blank?
+      extension = uploaded_io.original_filename.split('.')
+      filename = "#{Time.now.strftime('%Y%m%d%H%M%S')}.#{extension[-1]}"
+      filepath = "#{PIC_PATH}/groupbuys/#{filename}"
+      File.open(filepath, 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
+
+      session[:pic_url] = "/groupbuys/#{filename}"
+    end
   end
 
   def destroy
