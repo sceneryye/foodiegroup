54a4cf14cef5ae29fc96aa2a06c2e21b98bf1d48
killernova <enternicky@126.com>
2016-01-19 15:15:51 +0800

    bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@
 require 'rest-client'
 class EventsController < ApplicationController
 before_action :validate_user!, only: [:new, :edit, :update, :create, :destroy]
-before_action :set_event, only: [:new, :edit, :update, :create, :destroy]
+before_action :set_event, only: [:edit, :update, :destroy, :show]
   def index
     @events = Event.where(locale: session[:locale]).includes(:user)
 
