71d4ba8f5c6f398d537b012d14d19c2afda1a485
killernova <enternicky@126.com>
2016-01-22 19:02:41 +0800

    clear rials log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,11 @@ RailsOnForum::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # remove the 'Started get XXXXXX' in Rails Log
+  config.after_initialize do |app|
+    app.assets.logger = Logger.new('/dev/null')
+  end
+
   # Do not eager load code on boot.
   config.eager_load = false
 
