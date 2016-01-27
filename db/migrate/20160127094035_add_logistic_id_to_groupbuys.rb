9ba3f7321e2a9635b97994554b817c3d653257de
Scenery Ye <scenery.ye@gmail.com>
2016-01-27 11:07:15 +0800

    logistics

--- db/migrate/20160127094035_add_logistic_id_to_groupbuys.rb
+++ db/migrate/20160127094035_add_logistic_id_to_groupbuys.rb
@@ -1,4 +1,4 @@
-class AddLogisticsIdToGroupbuys < ActiveRecord::Migration
+class AddLogisticIdToGroupbuys < ActiveRecord::Migration
   def change
     add_column :groupbuys, :logistic_id, :int
     add_column :groupbuys, :status, :tinyint, default: 1
