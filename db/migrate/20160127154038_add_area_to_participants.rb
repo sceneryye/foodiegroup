e0a64b03aa9fa55c0edc3b1b86bbbc72c9d0bcf0
Scenery Ye <scenery.ye@gmail.com>
2016-01-28 10:34:28 +0800

    db

--- db/migrate/20160127154038_add_area_to_participants.rb
+++ db/migrate/20160127154038_add_area_to_participants.rb
@@ -1,6 +1,7 @@
 class AddAreaToParticipants < ActiveRecord::Migration
   def change
     add_column :participants, :area, :string
-    add_column :participants, :discount_id, :int
+    add_column :participants, :discount_id, :integer
+    add_column :participants, :quantity, :integer
   end
 end
