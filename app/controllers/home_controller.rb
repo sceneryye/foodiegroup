91593c180f7895e3a59166cc4d8bacca2b36b7ae
killernova <enternicky@126.com>
2016-02-18 15:42:58 +0800

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,23 +9,7 @@ class HomeController < ApplicationController
 		Rails.logger.info "---------------------------#{session[:openid]}"
     Rails.logger.info "---------------------------#{session[:mobile]}"
     Rails.logger.info "---------------------------#{current_user.try(:nickname)}"
-    if params[:openid]
-      session[:openid] = params[:openid]
-      session[:avatar] = params[:avatar]
-      session[:nickname] = params[:nickname]
-      user = User.where(:weixin_openid=>session[:openid])
-      if user.size>0
-        @user = user.first do |u|
-          u.avatar = session[:avatar]
-          u.nickname = session[:nickname]
-        end.save
-
-        login user.first
-        redirect_to root_path
-      else
-        redirect_to register_path
-      end
-    end
+    
 		@events = Event.where("locale='#{session[:locale]}' and recommend > ?", 0).includes(:user)
 		@groupbuys = Groupbuy.where("recommend>0").includes(:user)
 		@tags = Tag.where(locale: session[:locale]).limit(10)
