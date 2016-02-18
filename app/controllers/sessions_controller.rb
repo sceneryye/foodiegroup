df12906ca57284d8eb8b56a6a0c18fcf80c17cfb
killernova <enternicky@126.com>
2016-02-18 17:41:25 +0800

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,13 +25,13 @@ class SessionsController < ApplicationController
   def auto_login
     session[:return_url] = params[:return_url]
     state = rand(30000).to_s.ljust(5, '0')
-    redirect_url = CGI.escape 'http://vshop.trade-v.com/foodiegroup/sessions/callback'
+    redirect_url = CGI.escape 'http://vshop.trade-v.com/foodiegroup/sessions/callback?return_url=' + params[:return_url]
     url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{WX_APP_ID}&redirect_uri=#{redirect_url}&response_type=code&scope=snsapi_userinfo&state=#{state}#wechat_redirect"
     redirect_to url
   end
 
   def callback
-    return_url = session[:return_url]
+    return_url = session[:return_url] || params[:return_url]
     code = params[:code]
     now = Time.zone.now.to_i
     if Wechat.first.nil?
