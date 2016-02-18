7d6775d594c8883826cd8836ccc8aa42902c9b15
killernova <enternicky@126.com>
2016-02-18 15:28:34 +0800

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,9 @@ class HomeController < ApplicationController
 	def index
 		#微信入口页面
 
-		
+		Rails.logger.info "---------------------------#{session[:openid]}"
+    Rails.logger.info "---------------------------#{session[:mobile]}"
+    Rails.logger.info "---------------------------#{current_user.try(:nickname)}"
 		@events = Event.where("locale='#{session[:locale]}' and recommend > ?", 0).includes(:user)
 		@groupbuys = Groupbuy.where("recommend>0").includes(:user)
 		@tags = Tag.where(locale: session[:locale]).limit(10)
