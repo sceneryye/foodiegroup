5e14261e001e09faba75573a86433634d49a5934
killernova <enternicky@126.com>
2016-02-23 15:41:31 +0800

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ RailsOnForum::Application.routes.draw do
 
     namespace :admin do
     resources :reports
-      post "downorder", :on=>:collection
+      #post "downorder", :on=>:collection
       get '/users_list', to: 'reports#users_list'
       get '/groupbuys_list', to: 'reports#groupbuys_list'
       get '/topics_list', to: 'reports#topics_list'
