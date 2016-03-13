ecfb7a8f8e6c048a79edc99b3bb37eb48dd496f8
killernova <enternicky@126.com>
2016-03-13 18:11:12 +0800

    fix bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -47,7 +47,7 @@ class SessionsController < ApplicationController
       login user
      # session[:mobile] = user.mobile
      #Rails.logger.info "---------------return_url=#{return_url}"
-     # redirect_to return_url
+     return redirect_to return_url
     #elsif return_url.split('?').first.in? ['http://foodie.trade-v.com/register', 'http://foodie.trade-v.com/login']
      # data = get_user_info(openid, access_token)
       #session[:openid] = data["openid"]
@@ -63,40 +63,41 @@ class SessionsController < ApplicationController
       # session[:openid] = data["openid"]
       # session[:avatar] = data["headimgurl"]
       # session[:nickname] = data["nickname"]
+    else
       new_user = User.new weixin_openid: data["openid"], avatar: data["headimgurl"], nickname: data["nickname"]
       if new_user.save
         login new_user
-        redirect_to return_url || root_path
+        return redirect_to return_url || root_path
       else
-        redirect_to root_path(errmsg: 'Failed to create new user.')
-      end
-    end
-  end
+        return redirect_to root_path(errmsg: 'Failed to create new user.')
+     end
+   end
+ end
 
-  private
+ private
 
-  def get_auth_access_token code
-    get_url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=#{WX_APP_ID}&secret=#{WX_APP_SECRET}&code=#{code}&grant_type=authorization_code"
-    res_data_json = RestClient.get get_url
-    res_data_hash = ActiveSupport::JSON.decode res_data_json
-    Rails.logger.info res_data_hash
-    res_data_hash
-  end
+ def get_auth_access_token code
+  get_url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=#{WX_APP_ID}&secret=#{WX_APP_SECRET}&code=#{code}&grant_type=authorization_code"
+  res_data_json = RestClient.get get_url
+  res_data_hash = ActiveSupport::JSON.decode res_data_json
+  Rails.logger.info res_data_hash
+  res_data_hash
+end
 
-  def refresh_auth_access_token
-    refresh_token = Wechat.first.auth_refresh_token
-    
-    get_url = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=#{WX_APP_ID}&grant_type=refresh_token&refresh_token=#{refresh_token}"
-    res_data_json = RestClient.get get_url
-    res_data_hash = ActiveSupport::JSON.decode res_data_json
-    access_expires_at = Time.zone.now.to_i + res_data_hash["expires_in"].to_i
-    Wechat.first.update(auth_access_token: res_data_hash["access_token"], auth_access_token_expires_at: access_expires_at)
-    res_data_hash
-  end
+def refresh_auth_access_token
+  refresh_token = Wechat.first.auth_refresh_token
 
-  def get_user_info openid, access_token
-    get_url = "https://api.weixin.qq.com/sns/userinfo?access_token=#{access_token}&openid=#{openid}&lang=zh_CN"
-    res_data_json = RestClient.get get_url
-    res_data_hash = ActiveSupport::JSON.decode res_data_json
-  end
+  get_url = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=#{WX_APP_ID}&grant_type=refresh_token&refresh_token=#{refresh_token}"
+  res_data_json = RestClient.get get_url
+  res_data_hash = ActiveSupport::JSON.decode res_data_json
+  access_expires_at = Time.zone.now.to_i + res_data_hash["expires_in"].to_i
+  Wechat.first.update(auth_access_token: res_data_hash["access_token"], auth_access_token_expires_at: access_expires_at)
+  res_data_hash
+end
+
+def get_user_info openid, access_token
+  get_url = "https://api.weixin.qq.com/sns/userinfo?access_token=#{access_token}&openid=#{openid}&lang=zh_CN"
+  res_data_json = RestClient.get get_url
+  res_data_hash = ActiveSupport::JSON.decode res_data_json
+end
 end
